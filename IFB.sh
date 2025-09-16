24ed8c434d984c03b415a2ed35d0b2fab4f8d830
Tanvir Kaisar <tanvir.kaisar.tk@gmail.com>
2025-09-16 14:50:13 -0700

    main

--- IFB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# Executes the following programs to find an IFB using the heuristic 
-# and solves the problem optimally
-
-python3 config.py
-python3 hsc_ALNS_IFB.py
-python3 hsc_MIP_start.py
--- input_problem.py
+++
@@ -1,3 +0,0 @@
-W = 100  #The width of the available stocks (same for all stocks)
-d = [(45, 22), (38, 42), (25, 52),(11, 53), (12, 78)] #This is demand specification, expressed as a tuple: (number of board needed = b_i, width of that needed board = w_i)
-

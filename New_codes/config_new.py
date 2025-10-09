e0114329877d2503b9ee9239dbc98568901b2efb
Tanvir Kaisar <tanvir.kaisar.tk@gmail.com>
2025-10-09 13:50:33 -0700

    working

--- New_codes/config_new.py
+++ New_codes/config_new.py
@@ -5,7 +5,7 @@ rand_seed = 111
 rnd.seed(42)
 
 
-NODES = 28
+NODES = 29
 num_neighbors = min(round(NODES*0.2),10)
 SEARCH_MODE = "heap"                                                         #options are: lifo, fifo, mixed, heap
 run_dp = False

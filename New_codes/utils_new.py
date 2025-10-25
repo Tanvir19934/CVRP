42739e8fda9b2a08d25527bb687a50768f98b10f
Tanvir Kaisar <tanvir.kaisar.tk@gmail.com>
2025-10-24 22:38:09 -0700

    fixed

--- New_codes/utils_new.py
+++ New_codes/utils_new.py
@@ -587,8 +587,7 @@ class prize_collecting_tsp:
         
         self.m.setObjective(
             quicksum(w_ev*a[i,j]*self.x[i,j]  for i in V for j in V if i != j)   # base distance cost
-            + (theta-self.dual_values_subsidy)* quicksum(260*EV_cost*(a[i,j]/EV_velocity)*(gamma+gamma_l*(self.f[i,j])) for i in N for j in V if i!=j) + (theta-self.dual_values_subsidy)*quicksum(260*EV_cost*(a[0,j]/EV_velocity)*gamma*self.x[0,j] for j in N)
-            - quicksum(self.dual_values_delta[i]*self.y[i] for i in N)
+            + (theta-self.dual_values_subsidy)* quicksum(260*EV_cost*(a[i,j]/EV_velocity)*(gamma*self.x[i,j]+gamma_l*(self.f[i,j])) for i in V for j in V if i != j)
             - self.dual_values_vehicle
             - quicksum(self.dual_values_IR[i]*self.y[i]* (a[i,0]*GV_cost*q[i]+a[i,0]*GV_cost) for i in N)
             + - tol*0.001*(self.b['t']),     # to encourage the correct battery level at depot, otherwise Gurobi may set it to artificially small value to reduce cost
